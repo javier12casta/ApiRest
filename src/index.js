c5ede755f36052f5e60c712b7290750fdedf0461
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-07-30 06:13:10 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ app.set('port', process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // Routes
 app.use(require('./routes/Modalidad'));
